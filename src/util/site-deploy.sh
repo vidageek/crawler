d4d47576dabed4ca899822e38e8bfd1567f6587b
Jonas Abreu <jonas@vidageek.net>
2011-02-20 16:10:04 -0300

    adding site-deploy and redirection

---
+++ src/util/site-deploy.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -i
+
+ROOT=$PWD/../..;
+PROJECT="crawler"
+
+SITE_DIR=$ROOT/target/tubaina/html/$PROJECT;
+
+REPORTS_DIR=$ROOT/target/site;
+
+UTIL_DIR=$ROOT/src/util;
+
+SERVER=vidageek@vidageek.net:/home/vidageek/projetos.vidageek.net/$PROJECT/;
+
+cd $ROOT;
+
+mvn clean -o;
+
+#Generate english version of project's site
+mvn tubaina:build -o;
+
+#copy redirect file (Needed due to tubaina's structure)
+cp $UTIL_DIR/index.html $SITE_DIR/.;
+
+#copy files to server
+scp -rC $SITE_DIR/* $SERVER/.;
+
+echo "Please check the site and make sure everything is ok!";
+echo "Site deploy successful";
